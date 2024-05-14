f73b544bb38faaa4f07b8dfb6285e5018d64d887
Kaique Vieira <kvieiramiranda@gmail.com>
2024-05-14 13:32:48 -0300

    updates

--- src/main.py
+++ src/main.py
@@ -2,24 +2,8 @@
 Arquivo principal
 """
 
-import time, random
-from utils.utils import tempo_gasto
+from utils.utils import tempo_gasto, gerar_arquivo
 
-
-@tempo_gasto
-def gerar_arquivo(linhas: int, chaves_ordenadas: bool) -> None:
- nome_do_arquivo = f'arquivos_entrada/{linhas}_desordenado.txt'
- ids = random.sample(range(linhas), linhas)
-
- if chaves_ordenadas:
-   nome_do_arquivo = f'arquivos_entrada/{linhas}.txt'
-   ids = range(linhas)
-
- with open(nome_do_arquivo, "w") as arquivo:
-  for i in range(linhas):
-    linha = f'{ids[i]},12345,AOEUAOEUASTOEUHAOSENTUHAOSENTUHAS\n'
-    arquivo.write(linha)
-    
 
 def gerar_arquivos_facade(quantidade_de_linhas):
   for linhas in quantidade_de_linhas:
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,7 +2,7 @@
 Arquivo com funções uteis que podem ser reutilizadas
 """
 
-import time
+import time, random
 
 
 def tempo_gasto(funcao):
@@ -12,3 +12,17 @@ def tempo_gasto(funcao):
     tempo_final = time.time() - tempo_inicial
     print(f'{tempo_final:.6f}')
   return wrapper
+
+
+def gerar_arquivo(linhas: int, chaves_ordenadas: bool) -> None:
+  nome_do_arquivo = f'arquivos_entrada/{linhas}_desordenado.txt'
+  ids = random.sample(range(linhas), linhas)
+
+  if chaves_ordenadas:
+    nome_do_arquivo = f'arquivos_entrada/{linhas}.txt'
+    ids = range(linhas)
+
+  with open(nome_do_arquivo, "w") as arquivo:
+    for i in range(linhas):
+      linha = f'{ids[i]},12345,AOEUAOEUASTOEUHAOSENTUHAOSENTUHAS\n'
+      arquivo.write(linha)
